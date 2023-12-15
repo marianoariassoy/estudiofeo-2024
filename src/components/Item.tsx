dfdd136819d94ff8d04b08c78343daa385602d9a
marianoariassoy <hola@marianoarias.soy>
2023-12-15 19:31:18 -0300

    add about

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -6,12 +6,12 @@ const Item = ({ data }) => {
   return (
     <article className='relative w-full h-[100vw] lg:h-[70vh]'>
       <Link href={`/work/${data.id}/${sanitizeTitleForURL(data.title)}`}>
-        <button className='absolute top-0 left-0 w-full h-full bg-black/60 backdrop-blur z-10 cursor-pointer opacity-0 hover:opacity-100 transition-all'>
+        <button className='absolute top-0 left-0 w-full h-full lg:bg-black/20 lg:backdrop-blur z-10 cursor-pointer lg:opacity-0 hover:opacity-100 transition-all'>
           <div className='absolute bottom-0 left-0 w-full p-6 lg:p-12 text-white text-left'>
-            <h1 className='text-4xl lg:text-6xl font-extrabold uppercase mb-2'>
+            <h1 className='text-3xl lg:text-6xl font-extrabold uppercase'>
               {data.title}
             </h1>
-            <h2 className='text-5xl uppercase'> {data.subtitle}</h2>
+            <h2 className='text-2xl lg:text-4xl uppercase'> {data.subtitle}</h2>
           </div>
         </button>
       </Link>
